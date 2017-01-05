edf9873fee27aba7cf8583f1bebf6cb713531eef
Gustavo M T Borges <gustavo.mtborges@gmail.com>
2017-01-05 17:16:25 -0200

    improving aliases

--- zsh/example.zsh
+++ zsh/example.zsh
@@ -17,14 +17,17 @@ alias cc='xclip -selection clipboard'
 alias cv='xclip -o -selection clipboard'
 #
 # Start apps
-alias glassfish-start='cd ~/glassfish4/glassfish/bin; ./asadmin start-domain domain1 && cd -'
-alias glassfish-stop='cd ~/glassfish4/glassfish/bin; ./asadmin stop-domain domain1 && cd -'
-alias glassfish-log='cd ~/glassfish4/glassfish/domains/domain1/logs; cat server.log && cd -'
-alias glassfish-cleanLog='cd ~/glassfish4/glassfish/domains/domain1/logs; truncate -s0 server.log && cd -'
+
+## Glassfish 4
+alias glassfish-start='cd $HOME/glassfish4/glassfish/bin; ./asadmin start-domain domain1 && cd -'
+alias glassfish-stop='cd $HOME/glassfish4/glassfish/bin; ./asadmin stop-domain domain1 && cd -'
+alias glassfish-log='cd $HOME/glassfish4/glassfish/domains/domain1/logs; cat server.log && cd -'
+alias glassfish-cleanLog='cd $HOME/glassfish4/glassfish/domains/domain1/logs; truncate -s0 server.log && cd -'
+
+## MS Sql Server
 alias sql-server-start='systemctl start mssql-server'
 alias sql-server-stop='systemctl stop mssql-server'
 alias sql-server-status='systemctl status mssql-server'
-
 
 #
 # Locations
@@ -32,21 +35,20 @@ alias apache-sites='cd /var/www/html'
 alias list-svn='svn='http://ec2-54-164-48-81.compute-1.amazonaws.com/svn/projetos' && svn list --verbose http://ec2-54-164-48-81.compute-1.amazonaws.com/svn/projetos'
 #
 #
-# Connect
-alias aws-prod-gustavo='ssh -i ~/Dropbox/professional/gustavoborges.net/amazon-aws/aws-sao-paulo.pem ubuntu@54.207.34.41'
+# SSH connections
+alias aws-prod-gustavo='ssh -i $HOME/.keys/aws-sao-paulo.pem ubuntu@54.207.34.41'
 alias hostgator-heljur='ssh -p 2222 helpj728@216.172.172.49'
-alias aws-asert-devops='ssh -i ~/Dropbox/professional/asert/aws/application-server.pem ubuntu@ec2-54-164-48-81.compute-1.amazonaws.com'
-alias aws-asert-test='ssh -i ~/Dropbox/professional/asert/aws/asert-test-key.pem ubuntu@ec2-52-67-3-46.sa-east-1.compute.amazonaws.com'
-alias aws-nogueira='ssh -i ~/Dropbox/professional/nogueiranobre/aws/nogueiranobre-key.pem ubuntu@54.165.183.174'
+alias aws-asert-devops='ssh -i $HOME/.keys/application-server.pem ubuntu@ec2-54-164-48-81.compute-1.amazonaws.com'
+alias aws-asert-test='ssh -i $HOME/.keys/asert-test-key.pem ubuntu@ec2-52-67-3-46.sa-east-1.compute.amazonaws.com'
+alias aws-nogueira='ssh -i $HOME/.keys/nogueiranobre-key.pem ubuntu@54.165.183.174'
 #
 # Utils
 alias keyboard-restart='ibus-daemon -rd'
 #
 #
 # CUSTOM EXPORT
-
 export ANDROID_HOME=$HOME/android/sdk
 export PATH=$PATH:$JAVA_HOME/bin:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools
-export NVM_DIR="/home/gustavo/.nvm"
+export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
 
